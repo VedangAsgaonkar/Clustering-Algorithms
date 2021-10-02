da97f35aaf94db31b43b918724cc42c98acc33f7
Vedang Asgaonkar <vedanga2015@gmail.com>
2021-10-02 19:43:54 +0530

    density based estimator

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
-#include<iostream>
-#include<map>
-#include<iterator>
-#include<cmath>
-#include"point.h"
-#include"kMeansEstimator.h"
+#include <iostream>
+#include <map>
+#include <iterator>
+#include <cmath>
+#include "point.h"
+#include "kMeansEstimator.h"
+#include "densityBasedEstimator.h"
 
-int main(){
+int main()
+{
     srand48(12);
-    double* x_list = new double[400];
-    double* y_list = new double[400];
-    for(int i=0 ; i<400 ; i++){        
-        if(i<100){
-            x_list[i] = -5 + 4*drand48();
-            y_list[i] = -5 + 4*drand48();
+    double *x_list = new double[300];
+    double *y_list = new double[300];
+    for (int i = 0; i < 300; i++)
+    {
+        if (i < 200)
+        {
+            double theta = 6.28*drand48();
+            x_list[i] = 5*cos(theta);
+            y_list[i] = 5*sin(theta);
         }
-        else if(i<200){
-            x_list[i] = -5 + 4*drand48();
-            y_list[i] = 5 - 4*drand48();
-        }
-        else if(i<300){
-            x_list[i] = 5 - 4*drand48();
-            y_list[i] = -5 + 4*drand48();
-        }
-        else {
-            x_list[i] = 5 - 4*drand48();
-            y_list[i] = 5 - 4*drand48();
+        else if (i < 300)
+        {
+            double theta = 3.14/3 + 3.14*drand48()/3;
+            x_list[i] = -3*cos(theta);
+            y_list[i] = -3*sin(theta);
         }
     }
     std::cout << "type,x,y" << std::endl;
-    kMeansEstimator* k = new kMeansEstimator(x_list,y_list,400);
-    std::multimap<int,int> mp = k->cluster(4,20);    
-    for(auto i = mp.begin() ; i!=mp.end() ; i++){
-        std::cout << i->first << "," << x_list[i->second] << "," << y_list[i->second] << std::endl;
+    // kMeansEstimator *k = new kMeansEstimator(x_list, y_list, 400);
+    // std::multimap<int, int> mp = k->cluster(4, 20);
+    densityBasedEstimator *d = new densityBasedEstimator(x_list,y_list,300,1);
+    std::multimap<int, int> mp = d->cluster(3);
+    for (auto i = mp.begin(); i != mp.end(); i++)
+    {
+        std::cout << i->first << "," <<x_list[i->second] << "," << y_list[i->second] << std::endl;
     }
 }
---
+++ range_point.cpp
@@ -0,0 +1,20 @@
+#include "range_point.h"
+
+range_point::range_point() : point()
+{
+    num_neighbours = 0;
+    clustered = false;
+    index = -1;
+}
+range_point::range_point(double x, double y) : point(x, y)
+{
+    num_neighbours = 0;
+    clustered = false;
+    index = -1;
+}
+range_point::range_point(double x, double y, std::string label) : point(x, y, label)
+{
+    num_neighbours = 0;
+    clustered = false;
+    index = -1;
+}
